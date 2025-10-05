4d05a937399b1e20f03935309f3b109a1b4624f0
singhantariksh <antarikshsingh212@gmail.com>
2025-10-05 17:45:25 +0530

    fix: conform not detecting formatters :D

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,55 +1,56 @@
-local prettier_opts = { "prettierd", "prettier", stop_after_first = true }
-
 return {
 	"stevearc/conform.nvim",
 
-	opts = {
-		formatters_by_ft = {
-			-- Lua
-			lua = { "stylua" },
-
-			-- C/C++
-			c = { "clang-format" },
-			cpp = { "clang-format" },
-
-			-- C#
-			cs = { "csharpier" },
-
-			-- Python
-			python = { "isort", "black" },
-
-      -- Java
-      java = { "google_java_format" },
-
-			-- Rust
-			rust = { "rustfmt" },
-
-			-- Web
-			javascript = { prettier_opts },
-			javascriptreact = { prettier_opts },
-			typescript = { prettier_opts },
-			typescriptreact = { prettier_opts },
-			html = { prettier_opts },
-			css = { prettier_opts },
-
-			-- SQL
-			sql = { "sql_formatter" },
-
-			-- Mongo (JS/JSON format)
-			mongodb = { prettier_opts },
-
-			-- Other
-			go = { "gofmt" },
-			sh = { "shfmt" },
-			toml = { "taplo" },
-			yaml = { prettier_opts },
-			json = { prettier_opts },
-			xml = { prettier_opts },
-			markdown = { prettier_opts },
-		},
-	},
-
 	config = function()
+		local prettier_opts = { "prettierd", "prettier", stop_after_first = true }
+
+		require("conform").setup({
+			formatters_by_ft = {
+				-- Lua
+				lua = { "stylua" },
+
+				-- C/C++
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+
+				-- C#
+				cs = { "csharpier" },
+
+				-- Python
+				python = { "isort", "black" },
+
+				-- Java
+				java = { "google-java-format" },
+
+				-- Rust
+				rust = { "rustfmt" },
+
+				-- Web
+				javascript = prettier_opts,
+				javascriptreact = prettier_opts,
+				typescript = prettier_opts,
+				typescriptreact = prettier_opts,
+				html = prettier_opts,
+				css = prettier_opts,
+
+				-- SQL
+				sql = { "sql-formatter" },
+
+				-- Mongo (JS/JSON format)
+				mongodb = prettier_opts,
+
+				-- Other
+				go = { "gofmt" },
+				sh = { "shfmt" },
+				toml = { "taplo" },
+				yaml = prettier_opts,
+				json = prettier_opts,
+				xml = prettier_opts,
+				markdown = prettier_opts,
+			},
+		})
+
+		-- Keymaps
 		vim.keymap.set("n", "<leader>f", function()
 			require("conform").format({ async = true, lsp_fallback = true })
 		end, { desc = "Format file" })
