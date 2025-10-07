23015ff2d49ca900081a06b0d41e238b887abb91
singhantariksh <antarikshsingh212@gmail.com>
2025-10-07 17:37:16 +0530

    feat: added smart autopairs and some mini modules

---
+++ lua/plugins/mini-modules.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"echasnovski/mini.ai",
+		version = false,
+		opts = {},
+	},
+
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		opts = {},
+	},
+
+	{
+		"echasnovski/mini.splitjoin",
+		version = false,
+		opts = {},
+	},
+}
