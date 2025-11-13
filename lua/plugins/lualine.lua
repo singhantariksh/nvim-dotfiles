0add64f50e2ff2e7a0e42e8cbf99e4ca8b49d2f8
singhantariksh <antarikshsingh212@gmail.com>
2025-11-13 16:52:45 +0530

    feat(statusline): customized lualine to have time & lsp status with different seperators

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,28 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "auto",
-			component_separators = { left = "▕", right = "▏" },
-			section_separators = { right = "░▒▓", left = "▓▒░" },
-		},
-	},
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      theme = "auto",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff" },
+      lualine_c = { "diagnostics" },
+      -- lualine_c = { "%=", "filename" }, -- filename at the center of the statusline
+      lualine_x = { "encoding", "fileformat" },
+      lualine_y = { "lsp_status" },
+      lualine_z = { "os.date('%I:%M %p')" },
+    },
+    inactive_sections = {
+      lualine_a = { "filename" },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { "location" },
+    },
+  },
 }
