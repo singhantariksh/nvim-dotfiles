afe94c183fdb5dbbddda40ae87f514ab3f3ceb82
singhantariksh <antarikshsingh212@gmail.com>
2026-01-12 01:52:25 +0530

    feat(color): switched ccc for oklch-color-picker

---
+++ lua/plugins/color-picker.lua
@@ -0,0 +1,36 @@
+return {
+  "eero-lehtinen/oklch-color-picker.nvim",
+  event = "BufReadPost",
+  version = "*",
+  keys = {
+    {
+      "<leader>cc",
+      function()
+        require("oklch-color-picker").pick_under_cursor()
+      end,
+      desc = "Color pick under cursor",
+    },
+    {
+      "<leader>ci",
+      function()
+        require("oklch-color-picker").open_picker()
+      end,
+      desc = "Color insert",
+    },
+    {
+      "<leader>tc",
+      function()
+        require("oklch-color-picker").highlight.toggle()
+      end,
+      desc = "Toggle color highlight",
+    },
+  },
+  opts = {
+    highlight = {
+      enabled = true,
+      -- style = "foreground+virtual_left",
+      style = "background",
+      virtual_text = " ",
+    },
+  },
+}
