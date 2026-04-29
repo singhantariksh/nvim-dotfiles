2dd118bc5f0cdf10654d2dc3aeb9cb59a9bfedd4
singhantariksh <antarikshsingh212@gmail.com>
2026-04-29 19:36:33 +0530

    fix(ts): updated to treesitter new config requirements

--- lua/plugins/lsp-n-stuff/treesitter.lua
+++ lua/plugins/lsp-n-stuff/treesitter.lua
@@ -1,32 +1,207 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    { "nvim-treesitter/nvim-treesitter-textobjects", event = "VeryLazy" },
-    -- {
-    --   "nvim-treesitter/nvim-treesitter-context",
-    --   event = "BufReadPost",
-    --   opts = {
-    --     enable = true,
-    --     max_lines = 2, -- How many lines the window should span. Values <= 0 mean no limit.
-    --     multiline_threshold = 1, -- Maximum number of lines to show for a single context
-    --     trim_scope = "inner", -- Which context lines to discard if `max_lines` is exceeded. 'inner' | 'outer'
-    --   },
-    -- },
-  },
-  opts = {
-    auto_install = true,
-    sync_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+-- Auto-install parsers on file open for main branch
+vim.api.nvim_create_autocmd({ "Filetype" }, {
+  callback = function(event)
+    local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
+    if not ok then
+      return
+    end
 
-  config = function(_, opts)
-    local configs = require("nvim-treesitter.configs")
-    configs.setup(opts)
+    local parsers = require("nvim-treesitter.parsers")
+    if not parsers[event.match] or not nvim_treesitter.install then
+      return
+    end
 
-    -- vim.keymap.set("n", "<leader>tt", "<cmd>TSContext toggle<CR>", { desc = "Toggle treesitter context" })
+    local ft = vim.bo[event.buf].ft
+    local lang = vim.treesitter.language.get_lang(ft)
+
+    if not lang then
+      return
+    end
+
+    nvim_treesitter.install({ lang }):await(function(err)
+      if err then
+        vim.notify("Treesitter install error for ft: " .. ft .. " err: " .. err)
+        return
+      end
+
+      pcall(vim.treesitter.start, event.buf)
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    end)
   end,
+})
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    dependencies = {
+      { "folke/ts-comments.nvim", opts = {} },
+    },
+    branch = "main",
+    build = function()
+      if vim.fn.exists(":TSUpdate") == 2 then
+        vim.cmd("TSUpdate")
+      end
+    end,
+    config = function()
+      local ensure_installed = {
+        "bash",
+        "c",
+        "css",
+        "diff",
+        "gitcommit",
+        "html",
+        "javascript",
+        "lua",
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "vim",
+        "vimdoc",
+        "cpp",
+        "c_sharp",
+        "python",
+        "java",
+        "rust",
+        "typescript",
+        "tsx",
+        "sql",
+        "go",
+        "toml",
+        "yaml",
+        "json",
+        "xml",
+      }
+
+      local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
+      if not ok then
+        return
+      end
+
+      nvim_treesitter.install(ensure_installed)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+    branch = "main",
+    keys = {
+      {
+        "[f",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+        end,
+        desc = "prev function",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "]f",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+        end,
+        desc = "next function",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "[F",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+        end,
+        desc = "prev function end",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "]F",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+        end,
+        desc = "next function end",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "[a",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@parameter.outer", "textobjects")
+        end,
+        desc = "prev argument",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "]a",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@parameter.outer", "textobjects")
+        end,
+        desc = "next argument",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "[A",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_end("@parameter.outer", "textobjects")
+        end,
+        desc = "prev argument end",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "]A",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_end("@parameter.outer", "textobjects")
+        end,
+        desc = "next argument end",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "[s",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@block.outer", "textobjects")
+        end,
+        desc = "prev block",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "]s",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@block.outer", "textobjects")
+        end,
+        desc = "next block",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "[S",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_end("@block.outer", "textobjects")
+        end,
+        desc = "prev block",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "]S",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_end("@block.outer", "textobjects")
+        end,
+        desc = "next block",
+        mode = { "n", "x", "o" },
+      },
+      {
+        "gan",
+        function()
+          require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
+        end,
+        desc = "swap next argument",
+      },
+      {
+        "gap",
+        function()
+          require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner")
+        end,
+        desc = "swap prev argument",
+      },
+    },
+    opts = {
+      move = { enable = true, set_jumps = true },
+      swap = { enable = true },
+    },
+  },
 }
