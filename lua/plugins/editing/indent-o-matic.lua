cd8864aaf7c49451b582289a156d241df1654061
singhantariksh <antarikshsingh212@gmail.com>
2026-05-15 01:03:06 +0530

    feat(editing): indent-o-matic now only invoked manually

--- lua/plugins/editing/indent-o-matic.lua
+++ lua/plugins/editing/indent-o-matic.lua
@@ -6,4 +6,8 @@ return {
     standard_widths = { 2, 4, 8 },
     skip_multiline = true,
   },
+  config = function(_, opts)
+    require("indent-o-matic").setup(opts)
+    vim.api.nvim_clear_autocmds({ group = "indent_o_matic" })
+  end,
 }
